a52799a2c23c2a63e13af3bac88ff02c003e0112
globalmig <wdpb@mig.kr>
2026-01-13 11:40:23 +0900

    seo 최적화

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "가디언엔젤스 코리아",
   description: "가디언엔젤스 코리아입니다.",
+  keywords: [
+  "지역 사회 봉사",
+  "봉사활동",
+  "지역 사회 캠페인",
+  "안전 순찰",
+],
   openGraph: {
     title: '가디언엔젤스 코리아',
     description: '지역 사회의 안전을 지키기 위해 함께합니다.',
@@ -24,9 +30,12 @@ export const metadata: Metadata = {
     locale: 'ko_KR',
     type: 'website',
   },
-  other: {
-    'naver-site-verification': '',
-  }
+  verification: {
+    other: {
+      'naver-site-verification': '178ab10406fb53e943b8d5883c8a6cb60c32a12e',
+    }
+  },
+  robots: { index: true, follow: true },
 };
 
 export default function RootLayout({
